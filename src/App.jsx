6c0b8a53f321c9ab0c163b97795d41af9ea5f73d
toshiki.w_0120 <92259350+toshiki0120@users.noreply.github.com>
2024-05-22 03:08:57 +0900

    add:APIからデータを取得して表示できるようにする

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
+import { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+  const fetchCharacters = async () => {
+    const apiUrl = "https://narutodb.xyz/api/character";
+    const result = await axios.get(apiUrl);
+    setCharacters(result.data.characters);
+  };
+  return (
+    <div className="container">
+      <main>
+        <div className="cards-container">
+          {characters.map((character) => {
+            return (
+              <div className="card" key={character.id}>
+                <img
+                  src={
+                    character.images[0] != null
+                      ? character.images[0]
+                      : "dummy.png"
+                  }
+                  alt="character"
+                  className="card-image"
+                />
+                <div className="card-content">
+                  <h3 className="card-title">{character.name}</h3>
+                  <p className="card-description">
+                    {character.debut?.appearsIn ?? "なし"}
+                  </p>
+                  <div className="card-footer">
+                    <span className="affiliation">
+                      {character.personal?.affiliation ?? "なし"}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
